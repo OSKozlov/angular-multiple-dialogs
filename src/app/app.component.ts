efc634b6c346dd21d6f17537f5358b61022d8f71
Oleksandr Kozlov <dp070782kos@gmail.com>
2018-10-21 19:08:18 +0300

    angular multiple dialogs example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material';
+import { Dialog1Component } from './dialog1.component';
 
 @Component({
-  selector: 'app-root',
+  selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'app';
+
+  constructor(
+    public dialog: MatDialog
+  ) {}
+
+  openDialog() {
+    const dialogRef = this.dialog.open(Dialog1Component, {
+      id: 'dialog1'
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+import { Dialog1Component } from './dialog1.component';
+import { Dialog2Component } from './dialog2.component';
+
+import { MatButtonModule, MatDialogModule } from '@angular/material';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports:      [ BrowserModule, FormsModule, MatButtonModule, MatDialogModule, BrowserAnimationsModule ],
+  declarations: [ AppComponent, HelloComponent, Dialog1Component, Dialog2Component ],
+  entryComponents: [Dialog1Component, Dialog2Component],
+  bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
